22b63ad9b01018b6ecfac85a89f12c482c76ff6a
c0a24136de <c0a24136de@edu.teu.ac.jp>
2025-06-17 14:18:23 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random  #アルファベット順
 import sys
 import pygame as pg
 
@@ -19,8 +20,18 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect() 
-    # kk_rctという変数にget_rect()を入れる。その後、get_rect()の値を300,200に設定することで「こうかとん」がその座標に現れる
     kk_rct.center = 300, 200
+    # kk_rctという変数にget_rect()を入れる。その後、get_rect()の値を300,200に設定することで「こうかとん」がその座標に現れる
+
+    # 新しいsurfaceはループのwhile文の前に作る
+    bb_img = pg.Surface((20,20))  #空のSurfaceを作る
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)  #赤い円
+    bb_img.set_colorkey((0,0,0))
+    bb_rct = bb_img.get_rect()  #爆弾を作る
+    bb_rct.centerx = random.randint(0,WIDTH)  #横幅を超えないように設定する　center[x]という区切り　爆弾の設定
+    bb_rct.centery = random.randint(0,HEIGHT)  #縦幅を超えないように設定する center[y]という区切り　爆弾の設定
+    vx,vy = +5,+5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -34,7 +45,7 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
-        # key →　g.K_UPなど　mv　→ -= 5 の5の部分を指す
+        # key →　g.K_UPなどのkey　mv　→ -= 5 の5の部分を指す items()
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -45,9 +56,13 @@ def main():
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx,vy)
+        screen.blit(bb_img, bb_rct)  #爆弾の表示
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
+
 
 
 if __name__ == "__main__":
